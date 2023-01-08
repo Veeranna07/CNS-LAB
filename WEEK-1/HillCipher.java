0ed2b389ee395004be50c234bb64ff7936e873ae
BHUKYA VEERANNA <104159515+Veeranna07@users.noreply.github.com>
2023-01-08 18:52:16 +0530

    Update HillCipher.java

--- WEEK-1/HillCipher.java
+++ WEEK-1/HillCipher.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class HillCipher {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the 2x2 key matrix : ");
+        System.out.print("Enter the key matrix : ");
         int[][] key = new int[2][2];
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
